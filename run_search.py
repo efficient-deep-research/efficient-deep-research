701b8ab5346521c0733aed3879f7db8c45d10bb7
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2025-09-02 13:40:11 +0900

    Add `batch_size` and `max_length` kwargs to `ContextualAIReranker`

--- run_search.py
+++ run_search.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from search.rerankers import ContextualAIReranker
 from search.retrievers import FinewWebRetriever
@@ -7,15 +8,15 @@ from search.retrievers import FinewWebRetriever
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", type=str, required=True)
-    parser.add_argument("--instruction", type=str)
     parser.add_argument("--k", type=int, default=10)
+    parser.add_argument("--reranker_kwargs", type=str, default="{}")
     args = parser.parse_args()
 
     retriever = FinewWebRetriever(default_k=args.k)
     reranker = ContextualAIReranker()
 
     documents = retriever(args.query, args.k)
-    reranked_documents, scores = reranker(args.query, documents, instruction=args.instruction)
+    reranked_documents, scores = reranker(args.query, documents, **json.loads(args.reranker_kwargs))
 
     for i, document in enumerate(reranked_documents):
         print(f"Rank {i + 1} (Score: {scores[i]:.4f}):")
--- search/rerankers.py
+++ search/rerankers.py
@@ -42,18 +42,34 @@ class ContextualAIReranker(Reranker):
         return prompts
 
     def __call__(
-        self, query: str, documents: list[Document], instruction: str | None = None
-    ) -> tuple[list[Document], torch.tensor]:
+        self,
+        query: str,
+        documents: list[Document],
+        instruction: str | None = None,
+        max_length: int | None = None,
+        batch_size: int = 1,
+    ) -> tuple[list[Document], list[float]]:
         prompts = self.format_prompts(query, documents, instruction=instruction)
-        batch_encoding = self.tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(self.device)
 
-        with torch.no_grad():
-            model_output = self.model(**batch_encoding)
+        scores_list = []
+        for i in range(0, len(prompts), batch_size):
+            batch_prompts = prompts[i : i + batch_size]
+            batch_encoding = self.tokenizer(
+                batch_prompts, padding=True, truncation=True, max_length=max_length, return_tensors="pt"
+            )
+            with torch.no_grad():
+                batch_output = self.model(**batch_encoding.to(self.device))
 
-        scores = model_output.logits[:, -1, 0]
+            batch_scores = batch_output.logits[:, -1, 0]
+            scores_list.append(batch_scores.cpu())
+
+            del batch_encoding
+            del batch_output
+
+        scores = torch.cat(scores_list)
         sorting_idxs = scores.argsort(descending=True).tolist()
 
         sorted_documents = [documents[i] for i in sorting_idxs]
         sorted_scores = scores[sorting_idxs]
 
-        return sorted_documents, sorted_scores
+        return sorted_documents, sorted_scores.tolist()
