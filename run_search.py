b224acae170f44f28bcdf335fc50c04e99675d63
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2025-09-11 15:12:15 +0900

    Use `ClueWeb22Retriever` as default

--- run_search.py
+++ run_search.py
@@ -2,7 +2,7 @@ import argparse
 import json
 
 from search.rerankers import ContextualAIReranker
-from search.retrievers import FinewWebRetriever
+from search.retrievers import ClueWeb22Retriever
 
 
 if __name__ == "__main__":
@@ -12,14 +12,16 @@ if __name__ == "__main__":
     parser.add_argument("--reranker_kwargs", type=str, default="{}")
     args = parser.parse_args()
 
-    retriever = FinewWebRetriever(default_k=args.k)
+    retriever = ClueWeb22Retriever(default_k=args.k)
     reranker = ContextualAIReranker()
 
     documents = retriever(args.query, args.k)
     reranked_documents, scores = reranker(args.query, documents, **json.loads(args.reranker_kwargs))
 
     for i, document in enumerate(reranked_documents):
+        excerpt = document.text[:200].replace("\n", " ")
+
         print(f"Rank {i + 1} (Score: {scores[i]:.4f}):")
         print(f"  URL: {document.url}")
-        print(f"  Excerpt: {document.text[:200]}...")
+        print(f"  Excerpt: {excerpt}...")
         print("")
